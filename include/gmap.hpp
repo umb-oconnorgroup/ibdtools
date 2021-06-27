a7d2b71ac3cec6e6ef2d207a0f6a4664d442228e
Bing Guo <gbinux@gmail.com>
2021-06-26 21:32:57 -0400

    Fix GeneticMap precision issue
    Use llround to round off the get_bp calculation.
    
    Also used long double type of cM
    
    Noticed the possiblity that pos increases without cM increase (ie slope
    = 0). Provide a function to indicate whether a cM is located within
    these "horizontal regions"

--- include/gmap.hpp
+++ include/gmap.hpp
@@ -2,7 +2,9 @@
 #define __gmap_hpp__
 
 #include <algorithm>
+#include <cmath>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 
@@ -11,9 +13,9 @@
 class GeneticMap
 {
     int chrom_id;
-    std::vector<uint32_t> bp_pos_vec;
-    std::vector<float> cm_pos_vec;
-    std::vector<float> slope_vec; // cm per bp
+    std::vector<size_t> bp_pos_vec;
+    std::vector<long double> cm_pos_vec; // internal using unit 10^(-8) cM
+    std::vector<long double> slope_vec;  // cm per bp
 
   public:
     GeneticMap() {}
@@ -26,15 +28,15 @@ class GeneticMap
 
         std::string line, field;
         std::ifstream ifs(gmap_fn);
-        uint32_t bp_pos;
-        float cm_pos;
+        size_t bp_pos;
+        long double cm_pos;
         size_t line_counter = 0;
         while (std::getline(ifs, line, '\n')) {
             std::istringstream iss(line);
             std::getline(iss, field, ' ');
             std::getline(iss, field, ' ');
             std::getline(iss, field, ' ');
-            cm_pos = std::stod(field);
+            cm_pos = std::stold(field);
             std::getline(iss, field, ' ');
             bp_pos = std::stoul(field);
             add_position(bp_pos, cm_pos);
@@ -42,24 +44,88 @@ class GeneticMap
         add_final_slope();
     }
 
-    uint32_t
-    get_bp(float cm)
+    size_t
+    get_bp(long double cm)
     {
         auto id = std::distance(cm_pos_vec.begin(),
             std::upper_bound(cm_pos_vec.begin(), cm_pos_vec.end(), cm));
         id -= 1; // move to map point before the position
         auto slope = slope_vec[id];
-        return (cm - cm_pos_vec[id]) / slope + bp_pos_vec[id];
+
+        // if (llround((cm - cm_pos_vec[id]) / slope + bp_pos_vec[id]) > bp_pos_vec[id + 1]
+        //     && id < bp_pos_vec.size() - 2) {
+        //     print_range_info(id);
+        //     std::cout << "bp: "
+        //               << llround((cm - cm_pos_vec[id]) / slope + bp_pos_vec[id])
+        //               << " cm: " << cm << "\n---------\n";
+        // }
+        return llround((cm - cm_pos_vec[id]) / slope + bp_pos_vec[id]);
     }
 
-    float
-    get_cm(uint32_t bp)
+    // Like this region
+    // 10 . 82.484688 58212057
+    // 10 . 82.484688 58212123
+    // 10 . 82.484688 58212538
+    // 10 . 82.484688 58212655
+    bool
+    isin_horizontal_region(long double cm)
+    {
+        return std::distance(std::lower_bound(cm_pos_vec.begin(), cm_pos_vec.end(), cm),
+                   std::upper_bound(cm_pos_vec.begin(), cm_pos_vec.end(), cm))
+               > 1;
+    }
+
+    size_t
+    get_lower_id(size_t bp)
+    {
+        auto id = std::distance(bp_pos_vec.begin(),
+            std::upper_bound(bp_pos_vec.begin(), bp_pos_vec.end(), bp));
+        return id - 1;
+    }
+
+    size_t
+    get_lower_id(long double cm)
+    {
+        auto id = std::distance(cm_pos_vec.begin(),
+            std::upper_bound(cm_pos_vec.begin(), cm_pos_vec.end(), cm));
+        return id - 1;
+    }
+
+    void
+    print_range_info(size_t lower_id)
+    {
+        assert(lower_id >= 0);
+        if (lower_id >= bp_pos_vec.size() - 1) {
+            std::cout << std::setprecision(10) << "bp [" << bp_pos_vec.back() << ", Inf)"
+                      << " cm [" << cm_pos_vec.back()
+                      << ", Inf) with a slope: " << slope_vec.back() << '\n';
+        } else {
+            std::cout << "bp [" << bp_pos_vec[lower_id] << ", "
+                      << bp_pos_vec[lower_id + 1] << ")"
+                      << " cm [" << cm_pos_vec[lower_id] << ", "
+                      << cm_pos_vec[lower_id + 1]
+                      << ") with a slope: " << slope_vec[lower_id] << '\n';
+        }
+    }
+
+    long double
+    get_cm(size_t bp)
     {
         auto id = std::distance(bp_pos_vec.begin(),
             std::upper_bound(bp_pos_vec.begin(), bp_pos_vec.end(), bp));
         id -= 1; // move to map point before the position
         auto slope = slope_vec[id];
-        return (bp - bp_pos_vec[id]) * slope + cm_pos_vec[id];
+
+        // debug
+        // if (((bp - bp_pos_vec[id]) * slope + cm_pos_vec[id])
+        //     > cm_pos_vec[id + 1] + 0.0003) {
+        //     print_range_info(id);
+        //     std::cout << "bp: " << bp
+        //               << " cm: " << ((bp - bp_pos_vec[id]) * slope + cm_pos_vec[id])
+        //               << "\n---------\n";
+        // }
+
+        return ((bp - bp_pos_vec[id]) * slope + cm_pos_vec[id]);
     }
 
     void
@@ -96,16 +162,38 @@ class GeneticMap
         }
     }
 
+    size_t
+    get_first_nonzero_bp()
+    {
+        return bp_pos_vec[1];
+    }
+    size_t
+    get_last_bp()
+    {
+        return bp_pos_vec.back();
+    }
+    long double
+    get_first_nonzero_cm()
+    {
+        return cm_pos_vec[1];
+    }
+    long double
+    get_last_cm()
+    {
+        return cm_pos_vec.back();
+    }
+
   private:
     // add a poisition and update slope;
     void
-    add_position(uint32_t bp_pos, float cm_pos)
+    add_position(size_t bp_pos, long double cm_pos)
     {
         bp_pos_vec.push_back(bp_pos);
         cm_pos_vec.push_back(cm_pos);
         size_t sz = bp_pos_vec.size();
-        slope_vec.push_back((cm_pos_vec[sz - 1] - cm_pos_vec[sz - 2])
-                            / (bp_pos_vec[sz - 1] - bp_pos_vec[sz - 2]));
+        long double slope = (cm_pos_vec[sz - 1] - cm_pos_vec[sz - 2])
+                            / (bp_pos_vec[sz - 1] - bp_pos_vec[sz - 2]);
+        slope_vec.push_back(slope);
     }
 
     void
