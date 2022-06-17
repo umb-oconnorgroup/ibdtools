93d4edeeff9023e0fa2aba28e28a56861c97276d
Bing Guo <gbinux@gmail.com>
2022-06-17 19:03:56 -0400

    split ibdstat.hpp

--- include/ibdstat.hpp
+++ include/ibdstat.hpp
@@ -1,10 +1,7 @@
 #ifndef __ibdstat_hpp__
 #define __ibdstat_hpp__
-#include "common.hpp"
 #include "ibdfile.hpp"
 #include "metafile.hpp"
-#include "positions.hpp"
-#include <algorithm>
 
 class IbdStat
 {
@@ -14,53 +11,9 @@ class IbdStat
 
   public:
     IbdStat(const char *ibd_fn, const char *meta_fn, float genome_sz_cM = 3545.83,
-        size_t max_rec = 1 * 1024 * 1024)
-    {
+        size_t max_rec = 1 * 1024 * 1024);
 
-        BGZF *fp = bgzf_open(meta_fn, "r");
-        exit_on_false(fp != NULL, "can't open meta file", __FILE__, __LINE__);
-        meta.read_from_file(fp, false);
-        bgzf_close(fp);
-        fp = NULL;
-
-        ibd = IbdFile(ibd_fn, &meta, max_rec);
-
-        this->genome_sz_cM = ceil(genome_sz_cM);
-    }
-
-    std::vector<size_t>
-    get_stat()
-    {
-        auto &pos = ibd.get_meta()->get_positions();
-        auto &vec = ibd.get_vec();
-
-        std::vector<size_t> counter; // For return
-        counter.resize(genome_sz_cM * 1.5);
-
-        ibd.open("r");
-
-        bool read_full;
-        do {
-            read_full = ibd.read_from_file();
-            for (auto rec : vec) {
-                float cM = pos.get_cm(rec.get_pid2()) - pos.get_cm(rec.get_pid1());
-                counter[cM]++;
-            }
-
-        } while (read_full);
-
-        size_t i;
-        for (i = counter.size() - 1; i >= 0; i--) {
-            if (counter[i] != 0)
-                break;
-        }
-
-        counter.resize(i + 1);
-
-        ibd.close();
-
-        return counter;
-    }
+    std::vector<size_t> get_stat();
 };
 
 #endif
---
+++ src/ibdstat.cpp
@@ -0,0 +1,56 @@
+
+#include "ibdstat.hpp"
+#include "common.hpp"
+#include "ibdfile.hpp"
+#include "metafile.hpp"
+#include "positions.hpp"
+#include <algorithm>
+
+IbdStat::IbdStat(
+    const char *ibd_fn, const char *meta_fn, float genome_sz_cM, size_t max_rec)
+{
+
+    BGZF *fp = bgzf_open(meta_fn, "r");
+    exit_on_false(fp != NULL, "can't open meta file", __FILE__, __LINE__);
+    meta.read_from_file(fp, false);
+    bgzf_close(fp);
+    fp = NULL;
+
+    ibd = IbdFile(ibd_fn, &meta, max_rec);
+
+    this->genome_sz_cM = ceil(genome_sz_cM);
+}
+
+std::vector<size_t>
+IbdStat::get_stat()
+{
+    auto &pos = ibd.get_meta()->get_positions();
+    auto &vec = ibd.get_vec();
+
+    std::vector<size_t> counter; // For return
+    counter.resize(genome_sz_cM * 1.5);
+
+    ibd.open("r");
+
+    bool read_full;
+    do {
+        read_full = ibd.read_from_file();
+        for (auto rec : vec) {
+            float cM = pos.get_cm(rec.get_pid2()) - pos.get_cm(rec.get_pid1());
+            counter[cM]++;
+        }
+
+    } while (read_full);
+
+    size_t i;
+    for (i = counter.size() - 1; i >= 0; i--) {
+        if (counter[i] != 0)
+            break;
+    }
+
+    counter.resize(i + 1);
+
+    ibd.close();
+
+    return counter;
+}
