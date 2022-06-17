a4717727d6254e7b8be13c5179defd7601212b0d
Bing Guo <gbinux@gmail.com>
2022-06-17 18:11:20 -0400

    split metafile.hpp and ibdsplitter.hpp

---
+++ src/ibdsplitters.cpp
@@ -0,0 +1,181 @@
+
+#include "common.hpp"
+#include "ibdfile.hpp"
+#include "ibdspliter.hpp"
+#include "metafile.hpp"
+#include <algorithm>
+
+IbdSplitter::IbdSplitter(const char *in_fn, const char *out_fn_prefix,
+    const char *meta_fn, std::vector<region_label_t> &labels_, uint32_t max_label,
+    float cm_threshold_, size_t in_ibdrec_vec_capacity, size_t out_ibdrec_vec_capacity)
+    : labels(labels_), cm_threshold(cm_threshold_)
+{
+    BGZF *fp = bgzf_open(meta_fn, "r");
+    exit_on_false(fp != NULL, "", __FILE__, __LINE__);
+    meta.read_from_file(fp);
+    bgzf_close(fp);
+
+    std::string prefix = out_fn_prefix;
+    in = IbdFile(in_fn, NULL, in_ibdrec_vec_capacity);
+
+    // push an IbdFile for each label ( 0, 1, 2, ..., max_label)
+    for (uint32_t i = 0; i <= max_label; i++) {
+        std::string out_fn = out_fn_prefix + std::to_string(i);
+        out_files.push_back(IbdFile(out_fn.c_str(), NULL, out_ibdrec_vec_capacity));
+    }
+}
+
+void
+IbdSplitter::print_labels()
+{
+    for (auto label : labels) {
+        std::cout << "pid_s: " << label.pid_s << "\t label: " << label.label << '\n';
+    }
+}
+
+// driving code
+void
+IbdSplitter::split()
+{
+    in.open("r");
+    for (auto &f : out_files)
+        f.open("w");
+
+    auto &in_vec = in.get_vec();
+
+    bool read_full;
+    do {
+        read_full = in.read_from_file(false); // flush in info from input file
+
+        for (auto rec : in_vec) {
+
+            split_ibd_rec(rec); // split each record
+        }
+
+    } while (read_full); // if read_full is false, the input file reachs eof.
+
+    in.close();
+
+    // flush out ibd_vec to file
+    for (auto &f : out_files) {
+        if (f.get_vec().size() > 0) {
+            f.write_to_file();
+        }
+        f.close();
+    }
+}
+
+void
+IbdSplitter::split_ibd_rec(ibd_rec1_t rec)
+{
+    using namespace std;
+    uint32_t rid_s, rid_e;
+    uint32_t pid_l, pid_r, rid;
+
+    // ----------------
+    // get a range of region id
+    // ----------------
+    //
+    auto cmp_start_id
+        = [](region_label_t x, region_label_t y) -> bool { return x.pid_s < y.pid_s; };
+    auto lbeg = labels.begin();
+    auto lend = labels.end();
+    auto qleft = region_label_t{ rec.get_pid1(), 0 };
+    auto qright = region_label_t{ rec.get_pid2(), 0 };
+
+    rid_s = distance(lbeg, upper_bound(lbeg, lend, qleft, cmp_start_id));
+    rid_e = distance(lbeg, upper_bound(lbeg + rid_s, lend, qright, cmp_start_id));
+
+    ibd_rec1_t tmp_rec;
+    // ----------------
+    // Split by regions
+    // ----------------
+
+    /*                        rid_e
+                              |
+                              rid_s
+                              |
+
+        ------------++++++++++----------++++++++++-----------++++++++++
+                      ^----^
+
+                    /       \
+        rec.pid1        rec.pid2
+    */
+    // if rec is within a single split region
+    if (rid_s == rid_e) {
+        rid = rid_s - 1;
+        pid_l = rec.get_pid1();
+        pid_r = rec.get_pid2();
+
+        if (is_longer_than_2cm(pid_l, pid_r))
+            transfer(rec, rid);
+
+    } else {
+        /*                        s                                        e
+            ------------++++++++++----------++++++++++-----------++++++++++
+                             ^-----------------------------------------^
+
+                             |    |
+                             l    r
+        */
+        // for the left side, need to change pid2
+        rid = rid_s - 1;
+        pid_l = rec.get_pid1();
+        pid_r = labels[rid + 1].pid_s;
+
+        tmp_rec = rec;
+        tmp_rec.set_pid2(pid_r);
+        if (is_longer_than_2cm(pid_l, pid_r))
+            transfer(tmp_rec, rid);
+        /*                        s                                        e
+            ------------++++++++++----------++++++++++-----------++++++++++
+                             ^-----------------------------------------^
+
+                                  |         |
+                                  l         r
+        */
+        // in the middle, need to change both pid1 and pid2
+        for (rid = rid_s; rid < rid_e - 1; rid++) {
+            pid_l = labels[rid].pid_s;
+            pid_r = labels[rid + 1].pid_s;
+
+            tmp_rec = rec;
+            tmp_rec.set_pid1(pid_l);
+            tmp_rec.set_pid2(pid_r);
+            if (is_longer_than_2cm(pid_l, pid_r))
+                transfer(tmp_rec, rid);
+        }
+        /*  ------------++++++++++----------++++++++++-----------++++++++++
+                             ^-----------------------------------------^
+
+                                                                 |     |
+                                                                 l     r
+        */
+        // for the righ side, only need to change pid1
+        rid = rid_e - 1;
+        pid_l = labels[rid].pid_s;
+        pid_r = rec.get_pid2();
+
+        tmp_rec = rec;
+        tmp_rec.set_pid1(pid_l);
+        if (is_longer_than_2cm(pid_l, pid_r))
+            transfer(tmp_rec, rid);
+    }
+}
+
+void
+IbdSplitter::transfer(ibd_rec1_t rec, uint32_t rid)
+{
+    uint32_t label = labels[rid].label;
+
+    auto &f = out_files[label];
+    auto &out_vec = f.get_vec();
+
+    out_vec.push_back(rec);
+
+    if (out_vec.capacity() <= out_vec.size()) {
+        f.write_to_file(); // flush out info to output file
+        out_vec.clear();
+    }
+}
---
+++ src/metafile.cpp
@@ -0,0 +1,123 @@
+
+#include <htslib/bgzf.h>
+#include <htslib/vcf.h>
+
+#include <iostream>
+
+#include "chromosomes.hpp"
+#include "common.hpp"
+#include "genotypes.hpp"
+#include "gmap.hpp"
+#include "metafile.hpp"
+#include "positions.hpp"
+#include "samples.hpp"
+
+void
+MetaFile::parse_files(
+    const char *vcf_fn, const char *gmap_fn, bool parse_genotype, std::string chr_name)
+{
+    // ScopedTimer timer("vcffile-parse_files");
+
+    htsFile *htsfp = bcf_open(vcf_fn, "r");
+
+    exit_on_false(htsfp != NULL, "Cannot open vcf file", __FILE__, __LINE__);
+
+    bcf_hdr_t *header = bcf_hdr_read(htsfp);
+    exit_on_false(header != NULL, "Cannot read vcf header", __FILE__, __LINE__);
+    auto nsam = header->n[BCF_DT_SAMPLE];
+
+    genotypes = Genotypes(nsam);
+    chromosomes.add(chr_name, 0, 0);
+    positions = Positions(chromosomes.get_id(chr_name));
+    gmap = GeneticMap(-1, gmap_fn);
+
+    // add names to the Samples object
+    for (int32_t i = 0; i < nsam; i++)
+        samples.add(header->id[BCF_DT_SAMPLE][i].key);
+
+    bcf1_t *rec = bcf_init();
+    exit_on_false(rec != NULL, "Cannot initialize bcf record", __FILE__, __LINE__);
+
+    // loop over each record
+    int32_t *dest = NULL;
+    int32_t count = 0;
+    while (bcf_read(htsfp, header, rec) == 0) {
+        const int32_t max_ploidy = 2;
+
+        // 1. get positions
+        // This took a while to debug but bcftools internally use 0-based, while
+        // ibd/vcf use 1-based;
+        uint32_t bp_pos = rec->pos + 1;
+
+        // some vcf file have duplicated variants for a given position; Only the
+        // first variant at this position is considered for genotype information
+        bool success = positions.add(bp_pos, gmap.get_cm(bp_pos));
+
+        if (parse_genotype && success) {
+            // 2. get an array of alleles
+            bcf_get_genotypes(header, rec, &dest, &count);
+            exit_on_false(
+                max_ploidy * nsam == count, "Max ploidy is not 2", __FILE__, __LINE__);
+
+            // 3. loop over allele array and store the alleles
+            for (int32_t i = 0; i < count; i += 2) {
+                uint8_t allele[2];
+
+                allele[0] = bcf_gt_allele(dest[i]);
+                if (bcf_gt_is_missing(dest[i]))
+                    allele[0] = 2;
+
+                allele[1] = bcf_gt_allele(dest[i + 1]);
+                if (bcf_gt_is_missing(dest[i + 1]))
+                    allele[1] = 2;
+
+                genotypes.add(allele[0] | (allele[1] << 2));
+            }
+        }
+    }
+    if (dest != NULL)
+        free(dest);
+
+    bcf_hdr_destroy(header);
+    bcf_destroy(rec);
+    bcf_close(htsfp);
+}
+
+void
+MetaFile::write_to_file(BGZF *fp)
+{
+    chromosomes.write_to_file(fp);
+    gmap.write_to_file(fp);
+    samples.write_to_file(fp);
+    positions.write_to_file(fp);
+    genotypes.write_to_file(fp);
+}
+
+bool
+MetaFile::is_equal(MetaFile &other)
+{
+    return get_chromosomes().is_equal(other.get_chromosomes())
+           && get_genetic_map().is_equal(other.get_genetic_map())
+           && get_genotypes().is_equal(other.get_genotypes())
+           && get_positions().is_equal(other.get_positions())
+           && get_samples().is_equal(other.get_samples());
+}
+
+void
+MetaFile::read_from_file(BGZF *fp, bool read_genotype)
+{
+    chromosomes.read_from_file(fp);
+    gmap.read_from_file(fp);
+    samples.read_from_file(fp);
+    positions.read_from_file(fp);
+    if (read_genotype)
+        genotypes.read_from_file(fp);
+}
+
+void
+MetaFile::print()
+{
+    samples.print();
+    positions.print();
+    genotypes.print();
+}
